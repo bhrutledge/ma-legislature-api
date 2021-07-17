827a1c46fa1bb189a42f9fe133191c9d84106ae1
Brian Rutledge <brian@bhrutledge.com>
2021-07-17 13:12:26 -0400

    Add updated timestamp to legislation

--- notebooks/update_legislation.py
+++ notebooks/update_legislation.py
@@ -1,16 +1,20 @@
 import json
 
 import papermill as pm
+from tqdm import tqdm
 
 with open("../dist/legislation.json") as f:
     legislation_choices = json.load(f)
 
 bills = [bill for legislation in legislation_choices for bill in legislation["bills"]]
 
-for bill in bills:
-    print(bill)
+for bill in tqdm(bills):
     pm.execute_notebook(
         "Bill Details.ipynb",
         f"papermill/Bill Details {bill['session']} {bill['number']}.ipynb",
-        parameters=dict(bill_number=bill["number"]),
+        parameters=dict(
+            bill_session=bill["session"],
+            bill_number=bill["number"],
+        ),
+        progress_bar=False,
     )
